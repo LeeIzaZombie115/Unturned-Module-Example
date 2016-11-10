893f5fe53346a1ad2a46912326cd2a7916adcc1c
LeeIzaZombie115 <leeizazombie115@outlook.com>
2016-11-10 16:46:53 +0000

    Initial Release

---
+++ ExampleGUIClass.cs
@@ -0,0 +1,40 @@
+﻿using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ExampleModule
+{
+    public class ExampleGUIClass : PlayerCaller
+    {
+        public static ExampleGUIClass gui = null;
+        public int Counter = 0;
+
+        private void Start()
+        {
+            gui = this;
+            DontDestroyOnLoad(gui);
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.F7))
+            {
+                Counter++;
+            }
+        }
+
+        void OnGUI()
+        {
+            GUI.Window(0, new Rect(10, 10, 1000, 100), CreateWindow, "My Window...");
+        }
+
+        void CreateWindow(int id)
+        {
+            GUILayout.Label("Times F7 was pressed: " + Counter);
+        }
+    }
+}
---
+++ ExampleModule.cs
@@ -0,0 +1,28 @@
+﻿using SDG.Framework.Modules;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using UnityEngine;
+
+namespace ExampleModule
+{
+    public class ExampleModule : MonoBehaviour, IModuleNexus
+    {
+        public void initialize()
+        {
+            Player.onPlayerCreated += (Player player) =>
+                {
+                    player.transform.gameObject.AddComponent<ExampleGUIClass>();
+                };
+        }
+
+        public void shutdown()
+        {
+
+        }
+    }
+}
